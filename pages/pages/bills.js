c8e6fe40627f417ba8ba50508fbcd5c24b9a2b96
Lamido007 <149638491+Lamido007@users.noreply.github.com>
2025-12-13 02:45:41 +0100

    Delete broken bills.js to fix 404".

--- pages/pages/bills.js
+++
@@ -1,8 +0,0 @@
-export default function Bills() {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Bills Payment</h1>
-      <p>Electricity, DSTV, etc. (Full page coming soon)</p>
-    </div>
-  );
-}
